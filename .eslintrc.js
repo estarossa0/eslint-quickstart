42a8353c3de93b86c29093721243bad046c44f26
Rraji Abdelbari <rajiabdou9@gmail.com>
2023-06-15 20:17:24 +0100

    chore: removed unnecessary files

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
-
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  extends: [require.resolve('./config.js')],
-  parserOptions: {
-    project: './tsconfig.json',
-  },
-};
